d610c723e3be2e5efb7b7c6a607ee04e36f67c44
Natalia Prizon Tronco <priz0001@algonquinlive.com>
2025-06-19 13:34:51 -0500

    adds some css

--- scripts/sandwich.js
+++ scripts/sandwich.js
@@ -7,7 +7,7 @@ veggie: [],
 // store information about the bread as an object within this object
 bread: {
     kind: "", //an empty string
-    glutenfree: false
+    glutenFree: false
 }
 }
 
@@ -40,7 +40,7 @@ function addBread(){
 };
 
 document.querySelector('#gluten').addEventListener("change", function (){
-sandwich.bread.glutenfree =(this.checked) ? true: false;
+sandwich.bread.glutenFree =(this.checked) ? true: false;
 //console.log(sandwich);
 });
 
@@ -98,3 +98,33 @@ function addVeggie (){
     };
     console.log(sandwich);
 }
+
+//add an event listener to the form to see when it is submitted
+document.querySelector("form").addEventListener("submit", function(event){
+    //stop the form from submitting by stopping it's default behaviour
+    event.preventDefault();
+
+    //validate the form...
+    //first check if the user selected a breat type
+    if(sandwich.bread.kind == ""){
+        //use template literals to create the html for the error
+        const messageTemplate = `<p class="red">Your sandwich has to be on bread!</p>`
+        document.querySelector(".order").innerHTML = messageTemplate;
+    }else if(sandwich.protein.length == 0 && sandwich.veggie.length == 0){
+        //if both toppings arrays are empty, the user didn't select anything
+        const messageTemplate = `<p class="red">Your sandwich needs toppings!</p>`;
+        document.querySelector(".order").innerHTML = messageTemplate;
+    }else{
+        //they have both bread adn toppings, so confirm their order...
+        //create a message that repeats what they wanted
+
+        //turn our boolean values into readable text...
+        const toastedM = (sandwich.toasted) ? "toasted" : "";
+        const glutenM = (sandwich.bread.glutenFree) ? "gluten free" : "";
+
+        //build a message bvased on all the things in the object
+        const messageTemplate = `<p>You ordered a ${toastedM} sandwich on ${glutenM} ${sandwich.bread.kind} with ${sandwich.protein.join(",")} and ${sandwich.veggie.join(",")} </p>`;
+        document.querySelector(".order").innerHTML = messageTemplate;
+    }
+});
+
